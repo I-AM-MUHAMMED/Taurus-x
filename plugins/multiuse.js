46ec1b6324f8b71ba1a2f3f49a62d29187396cf1
Gizmo2003M <89608450+Gizmo2003M@users.noreply.github.com>
2021-08-27 14:01:47 +0100

    Update multiuse.js

--- plugins/multiuse.js
+++ plugins/multiuse.js
@@ -8,8 +8,8 @@ Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: IG_DESC }, async
 //if(match[1] == '') return 
     let { data, type } = await instaGram(match[1], '208c3e1637c946a6');
 //if(match[1] == '') return 
-    if (type === 'image') { await message.sendMessage(data, MessageType.image,{contextInfo: { forwardingScore: 2, isForwarded: true }, quoted: message.data,},{ caption: "Made By JulieMwol" }) }
-    else if (type === 'video') { await message.sendMessage(data, MessageType.video, {contextInfo: { forwardingScore: 2, isForwarded: true }, quoted: message.data}, { caption: "Made By JulieMwol" }) }
+    if (type === 'image') { await message.sendMessage(data, MessageType.image,{contextInfo: { forwardingScore: 2, isForwarded: true }, quoted: message.data,},{ caption: "made by Gizmo" }) }
+    else if (type === 'video') { await message.sendMessage(data, MessageType.video, {contextInfo: { forwardingScore: 2, isForwarded: true }, quoted: message.data}, { caption: "made by Gizmo" }) }
 });
 //const axios = require('axios')
 async function instaGram(url, key){

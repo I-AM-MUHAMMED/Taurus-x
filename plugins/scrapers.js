2418d6466dfb60c0197a9e392ecdf5d5529c0c3a
Hyper-sir <89091746+Hypersir@users.noreply.github.com>
2021-08-31 11:47:49 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -937,11 +937,12 @@ else if (config.WORKTYPE == 'public') {
                     .setFrame('APIC', {
                         type: 3,
                        reply = await message.client.sendMessage(message.jid,fs.readFileSync('./' + title + '.jpg'), MessageType.image, { caption: '*» PLAY SONG «*\n\n*» Title* : '+ title +'\n*» Ext* : MP3\n\n*_Please wait for the media file to be sent it may take a few minutes_*' });
+            await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.document, {filename: title + '.mp3', mimetype: 'audio/mpeg', contextInfo: { forwardingScore: 10, isForwarded: true }, quoted: message.data});
                         description: arama[0].description
                     });
                 writer.addTag();
 
-                reply = await message.client.sendMessage(message.jid,fs.readFileSync('./' + title + '.jpg'),MessageType.image,{ caption: '*» PLAY MUSIC «*\n\n*» Title* : '+ title +'\n*» Ext* : MP3 type Document\n\n*_Please wait for the media file to be sent it may take a few minutes_*' });
+                reply = await message.client.sendMessage(message.jid,fs.readFileSync('./' + title + '.jpg'), MessageType.image,{ caption: '*» PLAY MUSIC «*\n\n*» Title* : '+ title +'\n*» Ext* : MP3 type Document\n\n*_Please wait for the media file to be sent it may take a few minutes_*' });
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: false});
             });
     }));

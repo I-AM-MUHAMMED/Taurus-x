c8cc40e453227aa5b8253f70b8ef8932a9b59594
neera-j <86142113+neera-j@users.noreply.github.com>
2021-12-01 21:16:01 +0530

    LYDIA OPEN SSL ERROR FIXED

--- plugins/git.js
+++ plugins/git.js
@@ -13,22 +13,14 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('20210811_082543.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
  
-*owner number wa.me/918137829228*
+*owner number wa.me/918113921898*
    
-*owner number wa.me/918075379950*
+*owner number wa.me/918592061828*
+ 
+ *CONTACT MY OWNERS FOR SOURCE CODE*
 
 
-*whatsapp group : https://chat.whatsapp.com/FVOdpPLaMvP24rIfTzCGof*
 
-
-*githublink       _https://tinyurl.com/yjdej5a2_*
-
-
-*audio commads    _https://tinyurl.com/ydvlpbol_*
-
-*Bot Make Video ytube -https://youtu.be/s3ugOuxbQ_Q_*
-
-*sticker commads  _https://tinyurl.com/yfpd23yn_*     
 `}) 
 
 })); 

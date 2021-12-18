e206d9ddda0759ae24d56ef1ac0c29bfb3d46bc0
muhammed-usrbot <93420716+muhammed-usrbot@users.noreply.github.com>
2021-12-18 15:14:00 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,10 +12,10 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*TAURUS-BOT BY MUHAMMED'*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*TAURUS BOT BY MUHAMMED*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid, ' + Config.OWNER + '\n\n---------------------', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
         }
     }));
 }
@@ -30,10 +30,10 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*TAURUS-BOT BY MUHAMMED'*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*TAURUS BOT BY MUHAMMED*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid, ' + Config.OWNER + ' '\n\n--------------------', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
         }
     }));
 }

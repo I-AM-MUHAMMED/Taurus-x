9247557bdf355dca542527fcd6367367feb08172
muhammed-usrbot <93420716+muhammed-usrbot@users.noreply.github.com>
2021-12-18 23:38:20 +0530

    Update setup.js

--- plugins/setup.js
+++ plugins/setup.js
@@ -10,7 +10,7 @@ const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const hrs = new Date().getHours({ timeZone: 'Asia/Kolkata' })
 
-Badboy.addCommand({pattern: 'menu', fromMe: false, desc: 'it send bot menu'}, (async (message, match) => {
+Badboy.addCommand({pattern: 'git', fromMe: false, desc: 'it send bot menu'}, (async (message, match) => {
 
     var r_text = new Array ();
     

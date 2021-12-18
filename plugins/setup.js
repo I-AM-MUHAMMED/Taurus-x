1ae911ea5bea8b51840d237844bbb5fa9e0b106d
muhammed-usrbot <93420716+muhammed-usrbot@users.noreply.github.com>
2021-12-18 23:28:48 +0530

    Update setup.js

--- plugins/setup.js
+++ plugins/setup.js
@@ -1,1 +1,110 @@
+/* codded by raashii
+reedited by MUHAMMED
+use this git under copyright
+dont change credit
+*/
+
+const Badboy= require('../events');
+const config = require('../config');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const hrs = new Date().getHours({ timeZone: 'Asia/Kolkata' })
+
+Badboy.addCommand({pattern: 'menu', fromMe: false, desc: 'it send bot menu'}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+    r_text[0] = "https://bit.ly/3DOgLpP";
+    r_text[1] = "https://bit.ly/3cwa8fv";
+    r_text[2] = "https://bit.ly/3lnD2U8";
+    
+
+    var i = Math.floor(3*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    var time = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]
+
+    var wish = ''
+     
+    var eva = ''
+
+    var auto_bio = ''
+
+    var language = ''
+
+if (hrs < 12) wish = '*ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ ⛅*'
+if (hrs >= 12 && hrs <= 17) wish = '*ɢᴏᴏᴅ ᴀғᴛᴇʀɴᴏᴏɴ 🌞*'
+if (hrs >= 17 && hrs <= 19) wish = '*ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌥*'
+if (hrs >= 19 && hrs <= 24) wish = '*ɢᴏᴏᴅ ɴɪɢʜᴛ 🌙*'
+
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('20210811_082543.jpg'), mimetype: Mimetype.png, caption: `╭────────────────────╮
+   ` + config.BOTV2 + ` 
+╰────────────────────╯
+╭────────────────────╮
+│ ʜᴇʏ ᴜsᴇʀ ` + wish + `
+│         *⌚` + time + `
+│
+│
+┣𝕾⃝🌺 *ᴄʀᴇᴀᴛᴇʀ* : ᴍᴜʜᴀᴍᴍᴇᴅ
+┣𝕾⃝🌺 *ʙᴏᴛ* : ᴛᴀᴜʀᴜs
+┣𝕾⃝🌺 *ᴠᴇʀsɪᴏɴ* : 2.0.0
+┣𝕾⃝🌺 *ᴍᴏᴅᴇ* : ᴘᴜʙʟɪᴄ
+┣𝕾⃝🌺 *ᴘʀᴇғɪx* : *. ; !*
+│
+│      ▎▍▌▌▉▏▎▌▉▐▏▌▎
+│      ▎▍▌▌▉▏▎▌▉▐▏▌▎
+│       ©919961050829
+│
+│
+│☘︎ *ᴛᴏ ᴄʜᴇᴄᴋ ᴜᴘᴅᴀᴛᴇ ᴛʏᴘᴇ .ᴜᴘᴅᴀᴛᴇ*
+│
+│
+│
+│☘︎ *ʜᴏᴡ ᴛᴏ ᴜᴘᴅᴀᴛᴇ .ᴜᴘᴅᴀᴛᴇ ɴᴏᴡ*
+│
+│
+│
+│☘︎ *ʜᴏᴡ ᴛᴏ ᴍᴀᴋᴇ ʙᴏᴛ : 
+│ *https://t.ly/TGSb*
+│
+│
+│
+│☘︎ *ʜᴏᴡ ᴛᴏ ᴍᴀᴋᴇ ʜᴇʀᴏᴋᴜ ᴀᴄᴄᴏᴜɴᴛ : 
+│ *https://t.ly/coJ1*
+│
+│
+│
+│☘︎ *ɢɪᴛʜᴜʙ ʟɪɴᴋ :*
+│ *https://bit.ly/30GcHJS*
+│
+│
+│
+│☘︎ *ᴀᴜᴅɪᴏ ᴄᴏᴍᴍᴀɴᴅs :*
+│ *https://t.ly/oRpx*
+│
+│
+│
+│☘︎ *sᴛɪᴄᴋᴇʀ ᴄᴏᴍᴍᴀɴᴅs :* 
+│ *https://t.ly/ppzA*
+│
+│
+│
+│☘︎ *ᴡʜᴀᴛsᴀᴘᴘ ɢʀᴏᴜᴘ :*
+│ *https://bit.ly/3mcXDeb*
+│
+│
+│
+│☘︎ *ᴏᴡɴᴇʀ :*
+│ *https://bit.ly/3dZkOUC*
+│
+│ 
+│    ❏᪥ᴍᴜʜᴀᴍᴍᴇᴅ᪥❏
+╰────────────────────╯
+
+`}) 
+
+}));
 

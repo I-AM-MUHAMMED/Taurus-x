1997f6d97b2e48971d8c15831df6ea2a2fdaedff
muhammed-usrbot <93420716+muhammed-usrbot@users.noreply.github.com>
2021-12-19 15:34:37 +0530

    Update setup.js

--- plugins/setup.js
+++ plugins/setup.js
@@ -1,6 +1,7 @@
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
+const Config = require('../config');
 const config = require('../config');
 const fs = require("fs")
 const Language = require('../language');
@@ -8,7 +9,7 @@ const Lang = Language.getString('gitlink');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message, match) => {
 
-    var respoimage = await axios.get(config.LIZA, { responseType: 'arraybuffer' })
+    var respoimage = await axios.get(Config.LIZA, { responseType: 'arraybuffer' })
 
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('20210811_082543.jpg'), mimetype: Mimetype.png, caption: `╭────────────────────╮

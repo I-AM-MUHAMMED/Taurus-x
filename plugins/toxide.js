0f1c0496ba5f54a09b9957ddec736ee9da5dc365
MUHAMMED <93420716+I-AM-MUHAMMED@users.noreply.github.com>
2022-02-02 17:10:40 +0530

    15072052250

--- plugins/toxide.js
+++ plugins/toxide.js
@@ -1,17 +1,18 @@
 const Ktb = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
+const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');
 const Config = require('../config')
 Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
-//coded by muhammed
+//codded by muhammed
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:MUHAMMED [OWNER]\n' // full name
-            + 'ORG:MUHAMMED;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=919961050829:+919961050829\n' // WhatsApp ID + phone number
+            + 'FN:TAURUS BOT [OWNER]\n' // full name
+            + 'ORG:ᴛᴀᴜʀᴜs ʙᴏᴛ™;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=918157849715:+91 8157849715\n' // WhatsApp ID + phone number
+            + 'TEL;type=CELL;type=VOICE;waid=919961050829:+91 9961050829\n' // 
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "MUHAMMED [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "TAURUS BOT [OWNER]", vcard: vcard}, MessageType.contact, { mimetype: Mimetype.contact, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 99999, status: 200, thumbnail: fs.readFileSync('taurus.jpg'), surface: 200, message: Config.BOTV2, orderTitle: Config.BOTV2, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOTV2 + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('taurus.jpg')}}}});
 }))
